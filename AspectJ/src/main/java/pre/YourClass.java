da85b6c388ace588798c6267fa60a65514b779c5
Brigham Andersen <brighamband@gmail.com>
2021-11-10 23:40:11 -0700

    added files for aop tutorial

---
+++ AspectJ/src/main/java/pre/YourClass.java
@@ -0,0 +1,14 @@
+package pre;
+
+public class YourClass {
+
+    public static void main(String[] args) {
+        YourClass yourClass = new YourClass();
+        yourClass.yourMethodAround();
+    }
+
+    @YourAnnotation
+    public void yourMethodAround(){
+        System.out.println("Executing TestTarget.yourMethodAround()");
+    }
+}
